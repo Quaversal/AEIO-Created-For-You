898a18753f8463fa60208f9861cd144da9631c66
Connor Knauff <connor@aeiocreatedforyou.org>
2026-07-27 21:28:12 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,7 +9,17 @@ import SiteFooter from "@/components/SiteFooter";
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-stonebg">
+    <div className="relative min-h-screen bg-stonebg">
+      <div
+        className="pointer-events-none fixed inset-0 -z-10 bg-cover bg-center"
+        style={{
+          backgroundImage:
+            "url('https://media.base44.com/images/public/6a67b5b8cea8c1982f3940ce/f4b9080d5_image.png')",
+          filter: "blur(3px)",
+          transform: "scale(1.06)",
+        }}
+      />
+      <div className="pointer-events-none fixed inset-0 -z-10 bg-stonebg/65" />
       <SiteNav />
       <main>
         <Hero />
