96aa09a1be9301f6bbb457b8d269ada0fe471067
Connor Knauff <connor@aeiocreatedforyou.org>
2026-07-28 00:32:08 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -18,7 +18,7 @@ export default function Home() {
           transform: "scale(1.06)",
         }}
       />
-      <div className="pointer-events-none absolute top-0 inset-x-0 h-[850px] bg-gradient-to-b from-iceblue/35 via-iceblue/20 to-[#BFE4EE]" />
+      <div className="pointer-events-none absolute top-0 inset-x-0 h-[850px] bg-gradient-to-b from-iceblue/15 via-iceblue/10 to-[#BFE4EE]" />
       <SiteNav />
       <main className="relative z-10">
         <Hero />
